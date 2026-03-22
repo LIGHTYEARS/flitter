356bfe39f07b37fa646ea61a50d74c6c5bb2f7aa
ByteDance User <tiger@g340-cd52-700-c3d1-c735-796-4b9e.byted.org>
2026-03-22 10:15:07 +0800

    chore(09-02): export TextSpanHyperlink type from public API
    - Add type-only export of TextSpanHyperlink interface from src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ export { Color } from './core/color';
 export type { ColorMode } from './core/color';
 export { TextStyle } from './core/text-style';
 export { TextSpan } from './core/text-span';
+export type { TextSpanHyperlink } from './core/text-span';
 export { wcwidth, stringWidth } from './core/wcwidth';
 export { BoxConstraints } from './core/box-constraints';
 export { Key, ValueKey, UniqueKey, GlobalKey } from './core/key';

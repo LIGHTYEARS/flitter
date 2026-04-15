7916fd84e4c914a9c6623fea68a795e61077440f
洪尧舜 <hongyaoshun@bytedance.com>
2026-04-15 15:45:03 +0800

    fix(12.1-05): wire bin/flitter.ts entry point to call main()

--- packages/cli/bin/flitter.ts
+++ packages/cli/bin/flitter.ts
@@ -1,3 +1,9 @@
 #!/usr/bin/env bun
 // @flitter/cli - CLI entry point
-console.log("Flitter CLI placeholder");
+
+import { main } from "../src/main.js";
+
+main().catch((err) => {
+  process.stderr.write(`Fatal: ${err?.message ?? err}\n`);
+  process.exit(2);
+});
